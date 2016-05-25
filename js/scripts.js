82a2520d980c787c4760dac0480008f57d4ea52c
Walter Willand <wwilland@engr-131-18.eastern.ewu.edu>
2016-05-25 16:37:43 -0700

    A Buncha Shat

--- js/scripts.js
+++ js/scripts.js
@@ -20,42 +20,96 @@ $.simpleWeather({
     location: '99004',
     woeid: '',
     unit: 'f',
-    success: function(weather) {
-        
-      // Display Data
-      $('#cheney .city').text(weather.city);
-      $('#cheney .temp').text(weather.temp);
-    
-      // Display Icon
-      $('#cheney i').addClass( 'icon-' + weather.code );
-              
-      // Entire weather object
-      console.log(weather);
-    },
-    error: function(error) {
-      // Show if weather cannot be retreived
-    }
-  
-  });
+    success: function (weather) {
 
+        // Display Data
+        $('#cheney .city').text(weather.city);
+        $('#cheney .temp').text(weather.temp);
+
+        // Display Icon
+        $('#cheney i').addClass('icon-' + weather.code);
+        
+        // Get Forecast
+        $('#d1 .day').text(weather.forecast[1].date);
+        $('#d1 .temp').text(weather.forecast[1].low + ' - ' + weather.forecast[1].high);
+        $('#d1 i').addClass('icon-' + weather.forecast[1].code);
+
+        // Add Custom Body Class
+        if(weather.code >= 26 && weather.code <= 30) { 
+            $('body').removeClass(); 
+            $('body').addClass('bg-1'); 
+        }
+        
+        // Entire weather object
+        console.log(weather);
+    },
+    error: function (error) {
+        // Show if weather cannot be retreived
+    }
+
+});
+
+// Get Spokane Weather
 $.simpleWeather({
     location: '99201',
     woeid: '',
     unit: 'f',
-    success: function(weather) {
-        
-      // Display Data
-      $('#cheney .city').text(weather.city);
-      $('#cheney .temp').text(weather.temp);
-    
-      // Display Icon
-      $('#cheney i').addClass( 'icon-' + weather.code );
-              
-      // Entire weather object
-      console.log(weather);
+    success: function (weather) {
+
+        // Display Data
+        $('#spokane .city').text(weather.city);
+        $('#spokane .temp').text(weather.temp);
+
+        // Display Icon
+        $('#spokane i').addClass('icon-' + weather.code);
+
+        // Entire weather object
+        console.log(weather);
     },
-    error: function(error) {
-      // Show if weather cannot be retreived
+    error: function (error) {
+        // Show if weather cannot be retreived
     }
-  
-  });
+
+});
+
+// Get Geo Location **********
+if (!('geolocation' in navigator)) {
+    $('.geo button').show();
+} 
+else {
+    $('#geo').hide();
+    $('#geo').prepend('<p>Geo Location Not Supported</p>');
+}
+
+// On Click, Get Geolocation, Call Weather Function
+$('.geo button').click(function () {
+    //load weather using your lat/lng coordinates
+    navigator.geolocation.getCurrentPosition(function (position) {
+        getWeather(position.coords.latitude + ',' + position.coords.longitude);
+    });
+});
+
+// Wrap Plugin, in Function to be Called
+// Otherwise, runs when page loads
+var getWeather = function (location) {
+
+    $.simpleWeather({
+        location: location,
+        woeid: '',
+        unit: 'f',
+        success: function (weather) {
+
+            // Display Data
+            $('.geo .temp').text(weather.temp);
+            $('.geo .city').text(weather.city);
+
+            // Display Icon
+            $('.geo i').addClass('icon-' + weather.code);
+
+        },
+        error: function (error) {
+            // Show if weather cannot be retreived
+        }
+    });
+};
+// End Geo Location **********
